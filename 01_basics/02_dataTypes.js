8d0e471bb7fb10e912d0b30c80f7a94b95a969f3
Mr Amir <138612293+1234MrAmir@users.noreply.github.com>
2025-05-17 20:12:45 +0000

    Done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -26,5 +26,5 @@ let isLoggenOIn = false
 // console.log(typeof "Amir");
 console.log(typeof null); // object
 
-console.log(typeof undefined); // undefined
+console.log(typeof undefined); // undefined this is 
 
